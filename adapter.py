6d729c53d67332a6c2531914c23dfb1aeefcd9cc
CortoMaltese3 <giorgos.kalomalos@outlook.com>
2024-01-15 01:17:31 +0200

    chore/Add basic adapter file to manage the data transactions
    Add a basic adapter file to manage the possible CRUD operations that could be added in the future. This enhances the SOC and makes the stock_market.py leaner.

---
+++ adapter.py
@@ -0,0 +1,39 @@
+class DataAdapter:
+    def __init__(self):
+        # Sample data from the Global Beverage Corporation Exchange
+        self._stocks = {
+            "TEA": {
+                "Type": "Common",
+                "Last Dividend": 0,
+                "Fixed Dividend": 0,
+                "Par Value": 100,
+            },
+            "POP": {
+                "Type": "Common",
+                "Last Dividend": 8,
+                "Fixed Dividend": 0,
+                "Par Value": 100,
+            },
+            "ALE": {
+                "Type": "Common",
+                "Last Dividend": 23,
+                "Fixed Dividend": 0,
+                "Par Value": 60,
+            },
+            "GIN": {
+                "Type": "Preferred",
+                "Last Dividend": 8,
+                "Fixed Dividend": "2%",
+                "Par Value": 100,
+            },
+            "JOE": {
+                "Type": "Common",
+                "Last Dividend": 13,
+                "Fixed Dividend": 0,
+                "Par Value": 250,
+            },
+        }
+
+
+    def get_stocks(self):
+        return self._stocks
--- stock_market.py
+++ stock_market.py
@@ -1,41 +1,10 @@
 from datetime import datetime, timedelta
 
-
+from adapter import DataAdapter
 class StockMarket:
     def __init__(self) -> None:
-        # Sample data from the Global Beverage Corporation Exchange
-        self.stocks = {
-            "TEA": {
-                "Type": "Common",
-                "Last Dividend": 0,
-                "Fixed Dividend": 0,
-                "Par Value": 100,
-            },
-            "POP": {
-                "Type": "Common",
-                "Last Dividend": 8,
-                "Fixed Dividend": 0,
-                "Par Value": 100,
-            },
-            "ALE": {
-                "Type": "Common",
-                "Last Dividend": 23,
-                "Fixed Dividend": 0,
-                "Par Value": 60,
-            },
-            "GIN": {
-                "Type": "Preferred",
-                "Last Dividend": 8,
-                "Fixed Dividend": "2%",
-                "Par Value": 100,
-            },
-            "JOE": {
-                "Type": "Common",
-                "Last Dividend": 13,
-                "Fixed Dividend": 0,
-                "Par Value": 250,
-            },
-        }
+        self.data_adapter = DataAdapter()
+        self.stocks = self.data_adapter.get_stocks()
 
         # Trade log
         self.trades = []
